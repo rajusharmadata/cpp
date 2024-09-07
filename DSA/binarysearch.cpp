c2923e3bf627f7db054e2b8ccaebfaa73c08008c
raju <rajusharmadata123@gmail.com>
2024-09-07 19:27:10 +0530

    add

--- DSA/binarysearch.cpp
+++ DSA/binarysearch.cpp
@@ -1,25 +1,72 @@
-#include<bits/stdc++.h>
-#include <array>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-int binarysearch(int arr[],int key){
-   int start = 0; 
-   int last = 10;
-   int mid = (start+last)/2;
-   while(start<= last){
-    if(arr[mid]==key){
-        return mid;
+// without using function
+// int main()
+// {
+//     int arr[]  = {1,2,3,4,5,6,7,8,9,10};
+//     int key = 5;
+//     int beg = 0;
+//     int end = 9;
+//     int mid = (beg+end)/2;
+//     while(beg<=end && arr[mid]!=key)
+//     {
+//         if(key>arr[mid])
+//         {
+//             beg = mid+1;
+//         }
+//         else
+//         {
+//             end = mid-1;
+//         }
+
+//         mid = (beg+end)/2;
+//     }
+//     if(arr[mid]==key)
+//     {
+//         cout<<"index is : =  "<< mid;
+//     }else{
+//         cout<<"not found";
+
+//     }
+// }
+
+// using function
+int binarysearch(int arr[], int size, int key)
+{
+    int beg = 0;
+    int end = size - 1;
+
+    while (beg <= end)
+    {
+        int mid = (beg + end) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            beg = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
     }
-    if(arr[mid]<=last){
-        start = mid+1;
-    }else{
-        last = mid -1;
-    }
-   }
+    return -1;
 }
-
-int main(){
-    int arr[] = {4,5,6,8,10,12,14,20,25,40};
-    int key = 14;
-    cout<<binarysearch(arr,key);
+int main()
+{
+    int arr[] = {11, 22, 30, 40, 44, 55, 60, 66, 77, 80, 88, 99};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 40;
+    int index = binarysearch(arr, size, key);
+    if (index == -1)
+    {
+        cout << "This element  not exist in array " << endl;
+    }
+    else
+    {
+        cout << "This element is found at index : " << index << endl;
+    }
 }
